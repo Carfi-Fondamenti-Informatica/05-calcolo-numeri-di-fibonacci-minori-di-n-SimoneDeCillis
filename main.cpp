5dd69d519c73e41c4396f817704c2fed665c486d
SimoneDeCillis <116493653+SimoneDeCillis@users.noreply.github.com>
2022-11-28 08:17:23 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,22 @@
 using namespace std;
 
 int main() {
+      int n, a = 1, b = 1, c;
+    cin >> n;
+
+
+    cout << a << "\n" << b << "\n";
+
+    for (int i = 2; i<= n; i++) {
+
+        c = a + b;
+        
+        
+        a = b;
+        b = c;
+        cout << c << std::endl;
+
+    }
+
    return 0;
 }
